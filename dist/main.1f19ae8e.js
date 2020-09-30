ffa898ed6a17e2888746229f4c43fca1226152d1
Songsong <614750803@qq.com>
2020-09-30 21:46:21 +0800

    update

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58551" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7386" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
